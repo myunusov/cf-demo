6d52d7495bff885f94784c27d5dc10103cfe53bc
Maksim Iunusov <myunusov@luxoft.com>
2018-10-19 16:44:52 +0300

    Add configuration server as git config

--- src/main/kotlin/org/maxur/cfdemo/CfDemoApplication.kt
+++ src/main/kotlin/org/maxur/cfdemo/CfDemoApplication.kt
@@ -1,12 +1,23 @@
 package org.maxur.cfdemo
 
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.config.server.EnableConfigServer
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
 @EnableConfigServer
-class CfDemoApplication
+class CfDemoApplication {
+
+    private val log = LoggerFactory.getLogger(javaClass)
+
+    @Value("\${configuration.projectName}")
+    fun setProjectName(projectName: String) {
+        log.info("Project: $projectName")
+    }
+
+}
 
 fun main(args: Array<String>) {
     runApplication<CfDemoApplication>(*args)
